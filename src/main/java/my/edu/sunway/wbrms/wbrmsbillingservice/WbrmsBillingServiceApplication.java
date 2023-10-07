7bd9cf5edfcd8429e823a7d5bcf6e7771e9e56de
vrnsky <vrnsky@protonmail.ch>
2023-10-07 17:55:20 +0800

    Initial commit with a stub of service

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsbillingservice/WbrmsBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsBillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsBillingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsbillingservice/WbrmsBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsbillingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsBillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
